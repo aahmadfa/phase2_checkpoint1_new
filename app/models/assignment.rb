70b009e0f354776d576c60293c7d386d56c0e7c4
Sulaiyman <aahmadfa@andrew.cmu.edu>
2023-02-20 15:36:13 +0300

    Fix more syntax 6

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -31,20 +31,20 @@ class Assignment < ApplicationRecord
     #Callback
     before_create :end_employee_assignment
 
-    #Methods
-    def end_employee_assignment
-        current_assignment = employee.current_assignment
-        current_assignment.update_attribute(:end_date, self.start_date) unless current_assignment.nil?
-    end
+    # #Methods
+    # def end_employee_assignment
+    #     current_assignment = employee.current_assignment
+    #     current_assignment.update_attribute(:end_date, self.start_date) unless current_assignment.nil?
+    # end
 
-    def proper_dates
-        if start_date.nil?
-            errors.add(:start_date, message: "why no start date?")
-        elsif start_Date > Date.current
-            errors.add(:start_date, message: "must be on or before present date")
-        end
+    # def proper_dates
+    #     if start_date.nil?
+    #         errors.add(:start_date, message: "why no start date?")
+    #     elsif start_Date > Date.current
+    #         errors.add(:start_date, message: "must be on or before present date")
+    #     end
     
-        if end_date.present? && end_date < start_date
-            errors.add(:end_date, message: "must be after start date")
-        end
-    end
+    #     if end_date.present? && end_date < start_date
+    #         errors.add(:end_date, message: "must be after start date")
+    #     end
+    # end
