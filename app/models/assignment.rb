e8efeff29f48de5869c5791073b26da34bed4e0e
Sulaiyman <aahmadfa@andrew.cmu.edu>
2023-02-23 17:08:19 +0300

    Fixed the dates again 2

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -10,7 +10,7 @@ class Assignment < ApplicationRecord
     #dates
     validates :start_date, presence: true
     validates_date :start_date, date: { on_or_before: Date.today }
-    validate :proper_dates
+    validates_date :end_date, date: { on_or_after: self.start_date }
 
     # Scopes
     scope :current, -> { where(end_date: nil)} #current assignments
@@ -32,16 +32,3 @@ class Assignment < ApplicationRecord
         current_assignment = employee.current_assignment
         current_assignment.update_attribute(:end_date, self.start_date) unless current_assignment.nil?
     end
-
-    def proper_dates
-        if start_date.nil?
-            errors.add(:start_date, message: "why no start date?")
-        elsif start_date > Date.current
-            errors.add(:start_date, message: "must be on or before present date")
-        end
-    
-        if end_date.present? && end_date < start_date
-            errors.add(:end_date, message: "must be after start date")
-        end
-    end
-end
